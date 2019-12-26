561ecdc2b32a07b57b60680cd1d6b70b52b19afe
Albat.wang <84272166@qq.com>
2019-12-26 17:51:12 +0800

    XCode project init

---
+++ BDTradingAssistants/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  BDTradingAssistants
+//
+//  Created by Albatross on 2019/12/26.
+//  Copyright © 2019 Albatross. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
